7bbfb8fea2cbfc3af58ea847efb1f0273b97d3e8
Meghan Murphy <meghanmurphy.212@gmail.com>
2021-07-28 14:33:27 -0700

    added files and set up connection to server and database

---
+++ controllers/api/userRoutes.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+router.get('/', async (req,res) => {
+    try {
+        const userData = await User.findAll();
+        res.status(200).json(userData);
+
+    } catch (err) {
+        res.status.apply(400).json(err);
+    }
+})
+
+module.exports = router;
