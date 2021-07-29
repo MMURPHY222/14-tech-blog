82c9c3948f3ef2042b1cee4c6aef4202cf193f41
Meghan Murphy <meghanmurphy.212@gmail.com>
2021-07-29 15:21:23 -0700

    models index established relationships

--- models/index.js
+++ models/index.js
@@ -1,3 +1,34 @@
 const User = require('./User');
+const Comment = require('./comment');
+const Post = require('./post');
 
-module.exports = { User};
+User.hasMany(Post, {
+    foreignKey: 'user_id',
+    foreignKey: 'user_name',
+    onDelete: 'CASCADE'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+    foreignKey: 'user_name',
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+module.exports = { User, Comment, Post};
