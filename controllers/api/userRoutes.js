804d8e25e2d22c0ed3bc5bff41f3b338df28d2e7
Meghan Murphy <meghanmurphy.212@gmail.com>
2021-07-28 21:29:00 -0700

    established handlebars connection

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -4,7 +4,8 @@ const { User } = require('../../models');
 router.get('/', async (req,res) => {
     try {
         const userData = await User.findAll();
-        res.status(200).json(userData);
+        // res.status(200).json(userData);
+        res.render('homepage');
 
     } catch (err) {
         res.status.apply(400).json(err);
